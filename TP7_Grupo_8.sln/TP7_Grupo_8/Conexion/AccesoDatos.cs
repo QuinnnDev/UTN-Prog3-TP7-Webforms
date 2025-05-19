b581f3406b944e2fb208b1738ba46995c759931b
QuinnnDev <joacodvecchia@gmail.com>
2025-05-19 03:15:57 -0300

    Created AccesoDatos
    Clase nueva yippeeee

---
+++ TP7_Grupo_8.sln/TP7_Grupo_8/Conexion/AccesoDatos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace TP7_Grupo_8.Conexion
+{
+    public class AccesoDatos
+    {
+
+        string stringConexion = @"Data Source=localhost\\sqlexpress;Initial Catalog=BDSucursales;Integrated Security=True";
+
+        public SqlConnection ObtenerConexion()
+        {
+            SqlConnection sqlConnection = new SqlConnection(stringConexion);
+            try
+            {
+                sqlConnection.Open();
+                return sqlConnection;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+    }
+}
--- TP7_Grupo_8.sln/TP7_Grupo_8/SeleccionarSucursales.aspx.cs
+++ TP7_Grupo_8.sln/TP7_Grupo_8/SeleccionarSucursales.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using TP7_Grupo_8.Conexion;
 
 namespace TP7_Grupo_8
 {
